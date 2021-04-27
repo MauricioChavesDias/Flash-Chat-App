1a30249d63d3bac55284c8fec25246187fd0ef21
MauricioDias <mauriciodyas@gmail.com>
2021-04-27 14:17:25 +1000

    Added table view messages

---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,15 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Mauricio Dias on 27/4/21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+
+struct Message {
+    let sender: String
+    let body: String
+}
